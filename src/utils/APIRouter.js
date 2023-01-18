e914d0a9f267f4874ee6e83062ee3c7a4ab954a3
PRANDAVN\it_long <doanlong266@gmail.com>
2023-01-18 15:50:15 +0700

    fix api

--- src/utils/APIRouter.js
+++
@@ -1,4 +0,0 @@
-const host = "http://localhost:5000";
-export const registerRouter = `${host}/api/auth/register`;
-export const LoginRouter = `${host}/api/auth/login`;
-export const setAvatarRouter = `${host}/api/auth/setavatar`;
